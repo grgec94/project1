321095f2191836a866a2a4ec3dc4e6e75eb0afb8
MONO\praktikant <luka_94_an@hotmail.com>
2018-10-10 15:56:39 +0200

    properties for each role

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -6,7 +6,22 @@ namespace Project1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine("Available commands: Add, Remove, Display, List, < role_name > List");
+
+
+
+                Console.WriteLine("Role:");
+                string Role = Console.ReadLine();
+
+                Console.WriteLine("Frst Name:");
+                string FrstName = Console.ReadLine();
+
+                Console.WriteLine("Last Name:");
+                string LastName = Console.ReadLine();
+
+                Console.WriteLine("Age:");
+                string Age = Console.ReadLine();
+            
+               }
     }
 }
